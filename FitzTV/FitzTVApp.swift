2309e24e45288423ab66f4a907cd6ffc7b41ca79
Fitzgerald Afful <fitzafful@gmail.com>
2022-12-03 02:34:42 +0000

    Initial Commit

---
+++ FitzTV/FitzTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitzTVApp.swift
+//  FitzTV
+//
+//  Created by Fitzgerald Afful on 03/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct FitzTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
