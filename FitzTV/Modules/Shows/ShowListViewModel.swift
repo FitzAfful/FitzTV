34adaef34f40714f8a456760d40ce2c9f9d53c9e
Fitzgerald Afful <fitzafful@gmail.com>
2022-12-06 00:03:47 +0000

    Create view model for shows and add pagination

---
+++ FitzTV/Modules/Shows/ShowListViewModel.swift
@@ -0,0 +1,54 @@
+//
+//  ShowListViewModel.swift
+//  FitzTV
+//
+//  Created by Fitzgerald Afful on 04/12/2022.
+//
+
+import Foundation
+
+
+class ShowListViewModel: ObservableObject {
+    var repo: ShowRepository = ShowRepository()
+
+    @Published private(set) var isLoading = true
+    @Published var shows: [Show] = []
+    @Published var thrownError: Error? = nil
+    @Published var hasNextPage = false
+
+    @Published private(set) var currentPage: Int = 0
+
+    func loadData() {
+        Task {
+            await fetchShows()
+        }
+    }
+
+    @MainActor func fetchShows() async {
+        do {
+            self.isLoading = true
+            let newShows = try await repo.getShows(page: currentPage)
+            self.shows.append(contentsOf: newShows)
+            hasNextPage = !newShows.isEmpty
+            currentPage += 1
+            self.isLoading = false
+        } catch {
+            self.isLoading = false
+            thrownError = error
+        }
+    }
+
+    func loadMore(currentShow: Show) {
+        let thresholdIndex = self.shows.index(self.shows.endIndex, offsetBy: -7)
+        if let currentIndex = self.shows.firstIndex(of: currentShow) {
+            if currentIndex >= thresholdIndex && self.hasNextPage {
+                Task { await fetchShows() }
+            }
+        }
+    }
+
+    func clearError() {
+        thrownError = nil
+    }
+
+}
